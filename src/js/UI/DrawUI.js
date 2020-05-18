d3a06327affa99a2c6cbcc1813624ac23738761b
Alexey Lebedev <vorresti@ya.ru>
2020-05-18 18:14:46 +0300

    DrawUI init added

--- src/js/UI/DrawUI.js
+++ src/js/UI/DrawUI.js
@@ -1,9 +1,15 @@
 export default class DrawUI {
   constructor() {
+    this.title = document.getElementById('title');
     this.question = document.getElementById('question');
   }
 
+  init() {
+    this.title.innerText = 'выбери тему викторины';
+  }
+
   drawQuestion(question) {
+    this.title.innerText = 'ответь на вопрос';
     this.question.innerText = question;
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,34 @@ import DrawUI from './UI/DrawUI';
 
 const qaGenerator = new QAGenerator();
 const drawUI = new DrawUI();
+drawUI.init();
 
 let theme = null;
 
 const themesContainer = document.getElementById('themes');
+let hiddenElements = document.querySelectorAll('.hidden');
 
 themesContainer.addEventListener('click', (event) => {
   if (event.target.dataset.id) {
     theme = event.target.dataset.id;
     const question = qaGenerator.getQuestion(theme);
+
+    if (hiddenElements.length > 0) {
+      hiddenElements.forEach(element => element.classList.remove('hidden'));
+      hiddenElements = document.querySelectorAll('.hidden');
+    }
+
     drawUI.drawQuestion(question);
+    themesContainer.classList.add('hidden');
   }
 });
+
+const newGameButton = document.getElementById('new-game-button');
+
+newGameButton.addEventListener('click', () => {
+  theme = null;
+  hiddenElements = document.querySelectorAll('.hidden-init');
+  hiddenElements.forEach(element => element.classList.add('hidden'));
+  themesContainer.classList.remove('hidden');
+  drawUI.init();
+})
